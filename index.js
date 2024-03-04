8a0d8726b58fb6bae566a0a51c9b7c169ad5879b
John <jaricaya@gmail.com>
2024-03-03 17:47:37 -0800

    sending Data with Fetch lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+//makes a POST request to /users with a name and email
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email
+      }),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        document.querySelector('body').append(object.id);
+      })
+      .catch(error => {
+        console.error(error);
+        document.querySelector('body').append(error.message);
+      });
+  }
